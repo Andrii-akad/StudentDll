d52cbeb01ccb87f80fd6a1dec739db8f2cf46100
Andrii <zazik1323@gmail.com>
2019-12-14 17:31:12 +0200

    Second commit

---
+++ StudentLibrary/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    [Serializable]
+    public class Student
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public List<Mark> Marks { get; set; }
+        public Student()
+        {
+            Marks = new List<Mark>();
+        }
+        public override string ToString()
+        {
+            string rating = String.Empty;
+            foreach (var item in Marks)
+            {
+                rating += Environment.NewLine/*"\n"*/ + item; 
+            }
+            return $"Name: {Name}, Last Name: {LastName}, Age: {Age}, Rating: {rating}";
+        }
+        public void AddMark(string subj,int mark)
+        {
+            Marks.Add(new Mark{ Rating = mark, Subject = subj });
+        }
+        public double AverageMark()
+        {
+            return Marks.Average(x => x.Rating);
+        }
+    }
+    [Serializable]
+    public class Mark
+    {
+        public int Rating { get; set; }
+        public string Subject { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Subject} -------- {Rating}";
+        }
+        public Mark()
+        {
+            Rating = 0;
+            Subject = null;
+        }
+    }
+}//dopeek reflector
---
+++ StudentLibrary/StudentService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace StudentLibrary
+{
+    public class StudentService
+    {
+        private string path = "students.db";
+        public IEnumerable<Student> Students { get; set; }
+        public StudentService()
+        {
+            if (File.Exists(path))
+                Load();
+            else
+                Students = new List<Student>();
+        }
+        public void Add(Student st)
+        {
+            (Students as List<Student>).Add(st);
+        }
+        public void Remove(string lastname)
+        {
+            List<Student> temp = Students as List<Student>;
+           temp.Remove(temp.Find(st => st.LastName.Equals(lastname)));
+        }
+        public void Best()
+        {
+            Student tmp = new Student();
+            foreach (var item in Students)
+            {
+                if (item.AverageMark() > tmp.AverageMark())
+                {
+                    tmp = item;
+                }
+            }
+            Console.WriteLine(tmp);
+        }
+        public void Save()
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, Students);
+            }
+        }
+        private void Load()
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                Students = (List<Student>)bf.Deserialize(fs);
+            }
+        }
+    }
+}
